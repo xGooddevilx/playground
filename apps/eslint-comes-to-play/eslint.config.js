257000b874e683402e550038234f19b62a3c1665
Gooddevil <behroziyan.mhrn@gmail.com>
2025-01-17 21:35:20 +0330

    Implement eslint config & introduce some plugins to it

--- apps/eslint-comes-to-play/eslint.config.js
+++ apps/eslint-comes-to-play/eslint.config.js
@@ -1,28 +1,67 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from "typescript-eslint";
+import jsConfig from "@eslint/js";
+import globals from "globals";
+import unusedImports from "eslint-plugin-unused-imports";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+const baseConfig = () => {
+	return {
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		ignores: ["dist/**/*"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: {
+				...globals.es2025,
+				...globals.node,
+				...globals.browser,
+			},
+		},
+		rules: {
+			...jsConfig.configs.recommended.rules,
+			"no-unused-vars": [
+				"error",
+				{
+					args: "after-used",
+					argsIgnorePattern: "^_",
+					vars: "all",
+					varsIgnorePattern: "^_",
+				},
+			],
+		},
+		linterOptions: {
+			noInlineConfig: true,
+		},
+	};
+};
+
+const typescriptConfig = () => {
+	return [
+		...tseslint.configs.recommended,
+		{
+			languageOptions: {
+				parser: tseslint.parser,
+			},
+			plugins: {
+				tseslint: tseslint.plugin,
+			},
+
+			files: ["**/*.{ts,tsx}"],
+			rules: {
+				"@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+			},
+		},
+	];
+};
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	baseConfig(),
+	...typescriptConfig(),
+	{
+		plugins: {
+			"unused-imports": unusedImports,
+		},
+		rules: {
+			"unused-imports/no-unused-imports": "error",
+		},
+	},
+];
--- apps/eslint-comes-to-play/src/App.tsx
+++ apps/eslint-comes-to-play/src/App.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    console.log(window);
+  }, [])
 
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +26,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
