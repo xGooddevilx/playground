05c1a134d207a9c3d49cc55c8a2e3bed621999ea
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-03-19 13:22:15 +0330

    Practice async and sync file system

--- apps/node-pg/file-system/app.js
+++ apps/node-pg/file-system/app.js
@@ -1,17 +1,21 @@
+import { error } from "console";
 import fs from "fs";
 
 fs.writeFile("hello-world.txt", "Hello world", error => {
 	if (error) {
 		console.log("\x1b[31m%s\x1b[0m", "Failed to create the file");
 		console.error(error);
-        return;
+		return;
 	}
-    console.log('File created successfully!')
+	console.log("File created successfully!");
 });
 
+const textContent = fs.readFileSync("hello-world.txt", "utf-8");
+fs.writeFileSync("goodbye-world.txt", `This is input text = ${textContent}`);
 
-
-
-fs.writeFileSync('goodbye-world','Goodbye world')
-console.log('File created successfully synchronously');
-
+fs.readFile("./hello-world.txt", "utf-8", (error, data) => {
+	if (error) return;
+    fs.writeFile('./goodbye-world.txt','Content of this file is changed!!',"utf-8",(error)=>{
+        if(error) console.log('Failed to write in the file');
+    })
+});
