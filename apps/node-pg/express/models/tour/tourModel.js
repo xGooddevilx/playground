ba28b453ab2780160c3981a55748594035121abf
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-08-03 00:48:16 +0330

    inhance tour modeling

--- apps/node-pg/express/models/tour/tourModel.js
+++ apps/node-pg/express/models/tour/tourModel.js
@@ -1,18 +1,64 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Name is required"],
+    required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
-    required: [true, "Price is required"],
+    required: [true, "A tour must have a price"],
   },
-  rating: {
+  priceDiscount: {
     type: Number,
-    default: 4.5,
+    default: 0,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
   },
 });
 
